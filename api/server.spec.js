9ba5f5eebba14aaad9846e89b26e4806c40ba43f
blubrandi <blubrandi0@gmail.com>
2019-05-17 09:42:16 -0600

    Created jest.config.js

---
+++ api/server.spec.js
@@ -0,0 +1,22 @@
+const request = require('supertest')
+const server = require('./server.js')
+
+describe('Server', () => {
+
+    it('sets the environment to testing', () => {
+        expect(process.env.DB_ENV).toBe('testing')
+    })
+
+
+    describe('GET /', () => {
+        it('Should receive 200 response', () => {
+            return request(server)
+                .get('/')
+                .expect(200);
+        })
+    })
+
+
+
+
+})
